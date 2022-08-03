09bc7a46bb2b9863f4f603e97866bd83a6111b43
Victor Nunez <vnunez@live.ca>
2022-08-02 19:51:35 -0600

    Initial Commit

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const { sensorData } = require('./sensorData');
 const { date } = require('joi');
-const serialport = require('serialport');
-const SerialPort = serialport.SerialPort;
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require("@serialport/parser-readline");
+const { parse } = require('dotenv');
 require('dotenv').config();
 
 const app = express();
@@ -14,51 +15,41 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const serialport = new SerialPort({
+  path: "COM5",
+  baudRate: 9600,
+});
+
+const parser = serialport.pipe(new ReadlineParser({ delimiter: '\r\n'}));
+
 // Database Connection
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {}
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
+    
+      parser.on('data', (data) =>  {
 
-  var port = new SerialPort("/dev/ttyACM0"/*replace string with actual port number*/, {
-    baudrate: 9600,
-    parser: serialport.parsers.readline("\n")
-  });
+        const newestValue = JSON.parse(data);
+ 
+        const sensorDataPoint = {
+          email: "david.dentremont@edu.sait.ca",
+          temperature: newestValue.temp,
+          humidity: newestValue.humidity,
+          moistureLevel: newestValue.moisture,
+          date: new Date().toISOString(),
+        };
 
-  setInterval(() => {
+        sensorData.create(sensorDataPoint);
 
-    const environment = {
-      temperature: "",
-      humidity: "",
-      moistureLevel: "",
-    }
+        console.log("New Data Point Created");
 
-    // Read the port data
-    port.on("open", function () {
-      console.log('open');
-      port.on('data', function (data) {
-        console.log(data);
-        environment.temperature = data.temperature;
-        environment.humidity = data.humidity;
-        environment.moistureLevel = data.moistureLevel;
       });
-    });
-
-    const sensorDataPoint = {
-      email: "david.dentremont@edu.sait.ca",
-      temperature: environment.temperature,
-      humidity: environment.humidity,
-      moistureLevel: environment.moistureLevel,
-      date: new Date().getUTCDate(),
-    }
-
-    sensorData.create(sensorDataPoint);
-    console.log("Created new sensordata")
-  }, 10000);
 
   console.log("MongoDB database now connected successfully");
 });
+
 
 // Routes
 
